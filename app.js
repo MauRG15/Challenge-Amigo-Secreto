ded7bb569e6a68a88708f131fe1d87508c226bed
Mauricio Rojas Gallegos <mauriciorojasg15@gmail.com>
2025-08-20 17:28:15 -0600

    Añadir función para sotear los amigos

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-// Array donde se alamacenan los amigos que se van añadiendo.
-let amigos=[];
+// Variables
+let amigos=[];//Array donde se guardan los amigos que se registren
+let listahtml=document.getElementById('listaAmigos');//Variable para acceso a elemento "listaAmigos" en el HTML
 
 // Función para agregar un amigo a la lista
 // La función recibe un nombre y lo agrega al array de amigos
@@ -29,11 +30,31 @@ function limpiarCajaTexto(){
 
 //Función para agregar elementos a la lista en el HTML
 function agregarElementosLista(){
-    let listahtml=document.getElementById('listaAmigos');
-    listahtml.innerHTML='';//Evita que se dupliquen los datos de la lista
+    listahtml.innerHTML='';//Borra los datos ya alamacenados en la etiqueta <ul>, es decir todos los elementos <li> previammente creados.
+    //Ciclo para crear la lista de nuevo cada que se introduce un nuevo amigo, cada uno con su propia etiqueta <li>
     for (let i=0; i<amigos.length ; i++){
         let nuevoElementoLista=document.createElement('li');//Crear etiqueta en el DOM cada que se añade un elemento a la lista
         nuevoElementoLista.textContent=amigos[i];//Agregar texto a la etiqueta
         document.getElementById('listaAmigos').appendChild(nuevoElementoLista);//Indicar el lugar al que petenecerá dicha etiqueta
     }
+}
+
+//Función para sortear al amigo secreto
+function sortearAmigo(){
+    if (amigos.length>=2){
+        const amigoSecreto=Math.floor(Math.random()*amigos.length);
+        //console.log("Amigo sorteado:",amigos[amigoSecreto], "; Posicion en lista: ",amigoSecreto);//Verificar que el sorteo funcione
+        const resultadoHTML=document.getElementById('resultado');
+        resultadoHTML.innerHTML=`El amigo secreto es: ${amigos[amigoSecreto]}`;
+        limpiarLista();
+    }
+    else{
+        alert("Por favor, ingresa al menos 2 nombres a la lista para hacer el sorteo");
+    }
+}
+
+//Función para limpiar lista del HTML y JS para un nuevo juego
+function limpiarLista(){
+    amigos=[];
+    listahtml.innerHTML='';
 }
